934eed30a7d0d16ab0e1c32776b81a510bc6981b
Ana Luiza Santos <71670666+uaianaluiza@users.noreply.github.com>
2021-04-13 19:27:03 -0300

    Criar exemplo de teste unitário

---
+++ src/main/java/br/com/aula/junit/Agil.java
@@ -0,0 +1,23 @@
+package br.com.aula.junit;
+
+import java.sql.Struct;
+
+public class Agil {
+
+    public double calcularPert(double horasDificeis, double horasNormais, double horasFaceis){
+        if(validarNumerosPositivos(horasDificeis) || validarNumerosPositivos(horasFaceis)  ||
+                validarNumerosPositivos(horasNormais)){
+            throw new RuntimeException("Horas negativas não são aceitas");
+        }
+        double resultado = (horasDificeis + horasNormais + horasFaceis)/6;
+
+        return resultado;
+    }
+
+    public boolean validarNumerosPositivos(double numero){
+        if ( numero >=0){
+            return true;
+        }
+        return false;
+    }
+}
---
+++ src/test/java/br/com/aula/junit/AgilTest.java
@@ -0,0 +1,44 @@
+package br.com.aula.junit;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class AgilTest {
+    private Agil agil;
+
+    @Before
+    public void setup(){
+        this.agil = new Agil();
+    }
+
+    @Test
+    public void testarOCalculoCorretoDePiert(){
+        double horasDificeis = 3, horasFaceis = 3, horasNormais = 3;
+        double esperado = (horasDificeis+horasFaceis+horasNormais)/6;
+
+        //Resultado esperado 1,5
+        double resultado = agil.calcularPert(3,3,3);
+
+        Assert.assertEquals(esperado,resultado,0.004);
+    }
+
+    @Test
+    public void testarValidacaoDeNumeroNegativosParaHorasNoCalculoDePert(){
+
+       Exception erro = Assert.assertThrows(RuntimeException.class, () -> {
+            agil.calcularPert(4,-3,1);
+        } );
+       Assert.assertEquals("Horas negativas não são aceitas",erro.getMessage());
+    }
+
+    @Test
+    public void testarValidacaoDeNumeroPositivoCasoPositivo(){
+        Assert.assertTrue(agil.validarNumerosPositivos(10));
+    }
+
+    @Test
+    public void testarValidacaoDeNumeroPositivoCasoNegativo(){
+        Assert.assertFalse(agil.validarNumerosPositivos(-2));
+    }
+}
